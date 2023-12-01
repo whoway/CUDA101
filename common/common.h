dd556e7ba9cf1c2af101cd1c06242321888da0bc
whoway <whoway2017@qq.com>
2023-12-01 18:11:38 +0800

    [code]fix bug

--- common/common.h
+++ common/common.h
@@ -1,96 +1,17 @@
-#pragma once
-
-#include <fstream>
+#include <sys/time.h>
+#include <cuda_run_time.h>
 #include <stdio.h>
-#include <string>
-#include <vector>
 
-// whoway
-#define ErrorCheck(call)                                                                  \
-    do                                                                               \
-    {                                                                                \
-        const cudaError_t error_code = call;                                         \
-        if (error_code != cudaSuccess)                                               \
-        {                                                                            \
-            fprintf(stderr, "CUDA Error:\n");                                        \
-            fprintf(stderr, "    File:       %s\n", __FILE__);                       \
-            fprintf(stderr, "    Line:       %d\n", __LINE__);                       \
-            fprintf(stderr, "    Error code: %d\n", error_code);                     \
-            fprintf(stderr, "    Error text: %s\n", cudaGetErrorString(error_code)); \
-            exit(1);                                                                 \
-        }                                                                            \
-    } while (0)
-
-#define CHECK(call)                                                                  \
-    do                                                                               \
-    {                                                                                \
-        const cudaError_t error_code = call;                                         \
-        if (error_code != cudaSuccess)                                               \
-        {                                                                            \
-            fprintf(stderr, "CUDA Error:\n");                                        \
-            fprintf(stderr, "    File:       %s\n", __FILE__);                       \
-            fprintf(stderr, "    Line:       %d\n", __LINE__);                       \
-            fprintf(stderr, "    Error code: %d\n", error_code);                     \
-            fprintf(stderr, "    Error text: %s\n", cudaGetErrorString(error_code)); \
-            exit(1);                                                                 \
-        }                                                                            \
-    } while (0)
-
-#define CHECK_FILE_OPEN(call)                                  \
-    do                                                         \
-    {                                                          \
-        FILE *error_code = call;                               \
-        if (error_code == NULL)                                \
-        {                                                      \
-            fprintf(stderr, "OPEN FILE Error:\n");             \
-            fprintf(stderr, "    File:       %s\n", __FILE__); \
-            fprintf(stderr, "    Line:       %d\n", __LINE__); \
-            exit(1);                                           \
-        }                                                      \
-    } while (0)
-
-#define PRINT_SCANF_ERROR(count, n, text)                      \
-    do                                                         \
-    {                                                          \
-        if (count != n)                                        \
-        {                                                      \
-            fprintf(stderr, "Input Error:\n");                 \
-            fprintf(stderr, "    File:       %s\n", __FILE__); \
-            fprintf(stderr, "    Line:       %d\n", __LINE__); \
-            fprintf(stderr, "    Error text: %s\n", text);     \
-            exit(1);                                           \
-        }                                                      \
-    } while (0)
-
-#define PRINT_INPUT_ERROR(text)                            \
-    do                                                     \
-    {                                                      \
-        fprintf(stderr, "Input Error:\n");                 \
-        fprintf(stderr, "    File:       %s\n", __FILE__); \
-        fprintf(stderr, "    Line:       %d\n", __LINE__); \
-        fprintf(stderr, "    Error text: %s\n", text);     \
-        exit(1);                                           \
-    } while (0)
-
-#define PRINT_KEYWORD_ERROR(keyword)                                               \
-    do                                                                             \
-    {                                                                              \
-        fprintf(stderr, "Input Error:\n");                                         \
-        fprintf(stderr, "    File:       %s\n", __FILE__);                         \
-        fprintf(stderr, "    Line:       %d\n", __LINE__);                         \
-        fprintf(stderr, "    Error text: '%s' is an invalid keyword.\n", keyword); \
-        exit(1);                                                                   \
-    } while (0)
-
-#ifdef STRONG_DEBUG
-#define CUDA_CHECK_KERNEL               \
-    {                                   \
-        CHECK(cudaGetLastError());      \
-        CHECK(cudaDeviceSynchronize()); \
+cudaError_t ErrorCheck(cudaError_t status,
+                       const char *filename, int lineNumber)
+{
+    if (status != cudaSuccess)
+    {
+        printf("CUDA API error:\r\n");
+        printf("code=%d, name=%s, description=%s\r\n",
+               status, cudaGetErrorName(status), cudaGetErrorString(status));
+        printf("File: %s, Line=%d\r\n", filename, lineNumber);
     }
-#else
-#define CUDA_CHECK_KERNEL          \
-    {                              \
-        CHECK(cudaGetLastError()); \
-    }
-#endif
+
+    return status;
+}
